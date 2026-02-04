3273bb3f014710f428b92f789be749313c4b7df4
AkinyA <nyaraja398@gmail.com>
2026-02-04 15:04:01 +0300

    mise en place des packages
    Package pour la connection à la base de donnée et un script.sql pour tester la base

---
+++ src/main/java/com/workflow/projet/config/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package com.workflow.projet.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/projet_1_s6";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("Driver PostgreSQL non trouvé", e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
